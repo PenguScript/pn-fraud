b074f92632c828add61316eb10ba86b40f04a57b
PenguScripts <102926606+PenguScript@users.noreply.github.com>
2023-06-06 19:40:28 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.PSDispatch = false
+
+Config.FraudCheckModel = "a_m_y_business_03"
+Config.FraudCheckPosition = vector4(283.8, 265.19, 104.59, 337.05)
+Config.FraudCheckIcon = 'fa fa-money'
+Config.FraudCheckName = "???"
+
+
+Config.FraudMoneyRewardType = "cash"
+Config.FraudMoneyRewardValue = { min = 500, max = 1500 } -- min,max
+
+Config.RealCheckMoneyRewardType = "cash"
+Config.RealCheckMoneyRewardValue = { min = 250, max = 800 } -- min,max
---
+++ server/main.lua
@@ -0,0 +1,48 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+RegisterNetEvent('pn-fraud:ServerPrint', function(msg)
+    print(tostring(msg))
+end)
+
+QBCore.Functions.CreateCallback('pn-fraud:GetChecks', function(source, cb)
+
+    local items = {
+        [1] = {qty = 0, itemname = 'fraudcheck'},
+        [2] = {qty = 0, itemname = "check"}
+    }
+
+    local Player = QBCore.Functions.GetPlayer(source)
+    for k,v in pairs(Player.PlayerData.items) do
+        for key, info in pairs(items) do
+            if v.name == info.itemname then
+                info.qty = info.qty + v.amount
+            else
+                info.qty = info.qty + 0
+            end
+        end
+    end
+    cb(items)
+end)
+
+RegisterNetEvent('pn-fraud:GiveMoneyFromChecks', function(args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local ctype = args.type
+    local amnt = args.amnt
+    if ctype or amnt ~= nil or 0 then
+        if ctype == "fraud" then
+            local v1 = math.random(Config.FraudMoneyRewardValue.min,Config.FraudMoneyRewardValue.max)
+            local value = v1*amnt
+            Player.Functions.RemoveItem('fraudcheck', amnt)
+            Player.Functions.AddMoney(Config.FraudMoneyRewardType, value)
+        elseif ctype == "real" then
+            local v1 = math.random(Config.RealCheckMoneyRewardValue.min,Config.RealCheckMoneyRewardValue.max)
+            local value = v1*amnt
+            Player.Functions.RemoveItem('check', amnt)
+            Player.Functions.AddMoney(Config.RealCheckMoneyRewardType, value)
+        else
+            local msg = "There's an unknown error somewhere in this script. Please notify PenguScripts in a support ticket!"
+            TriggerEvent('pn-fraud:ServerPrint', msg)
+        end
+    end
+end)
